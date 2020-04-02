94d0d9d4ca1c7806f013ad47fa09270017aa6bf8
vpetrykov <vpetrikov15@gmail.com>
2020-04-02 16:27:54 +0200

    node js server + axios(not fully working)

--- src/Map.js
+++ src/Map.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import './App.css';
-
+import axios from 'axios';
 
 class Map extends React.Component{
+
+    state = {
+        messages : []
+    }
+
+
+
+    fetchMessages(){
+       console.log('before');
+        axios.get('localhost:3000/',{
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+            },
+            proxy: {
+                host: 'localhost:3306',
+                port: 3306
+            }
+        }) .then(res=>{
+            console.log('status: ' + res.data);
+        });
+
+    }
 
     render(){
         return (
@@ -10,6 +32,9 @@ class Map extends React.Component{
                 <div className="body border">
                     <h2>Should show map with drones</h2>
                 </div>
+
+                <button onClick={this.fetchMessages}>Click me</button>                                
+
             </div>
         );
     }
---
+++ src/routes.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+const app = express();
+
+let requestNum =0;
+
+// app.use(bodyParser.json({type:'application/json'}));
+// app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(function(req,res,next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+const connection = mysql.createPool({
+    host:'localhost:3306',
+    user:'monty',
+    password:'some_pass',
+    database:'KekistanDB'
+  });
+
+app.get('/', function (req, res) {
+    requestNum++;
+    console.log("reqeust #" + requestNum);
+    connection.getConnection(function (err, connection) {
+    connection.query('SELECT * FROM Messages', function (error, results, fields) {
+      if (error) throw error;
+      response.headers.add('Access-Control-Allow-Origin', '*');
+      res.send(results);
+    });
+  });
+});
+
+// Starting our server.
+app.listen(3000, () => {
+ console.log('app.listen 3000');
+});
