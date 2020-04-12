0a9e5954bed65e9ec07c73c7e03ff1312ef66766
vpetrykov <vpetrikov15@gmail.com>
2020-04-12 12:44:22 +0200

    deleted comments

--- src/routes.js
+++ src/routes.js
@@ -5,17 +5,7 @@ var cors = require('cors');
 
 const app = express();
 
-
 let requestNum =0;
-
-// app.use(bodyParser.json({type:'application/json'}));
-// app.use(bodyParser.urlencoded({extended:true}));
-
-// app.use(function(req,res,next){
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//     next();
-// });
 
 app.use(cors());
 
